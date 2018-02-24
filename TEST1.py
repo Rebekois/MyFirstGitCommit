e32b26ffd47c9a9f22b1b73ac5e7778098261d6b
Rebeko Ilia <Rebekois.hb@gmail.com>
2018-02-24 15:32:13 +1000

    Firs commit

---
+++ TEST1.py
@@ -0,0 +1,6 @@
+def funcTEST(par1, par2):
+	if par1 > par2:
+		print( par1," больше ",par2)
+	else:
+		print( par1,"меншье",par2)
+	pass
---
+++ TEST2.py
@@ -0,0 +1,6 @@
+from TEST1 import funcTEST
+
+x = input( "Введите первое число: " )
+y = input( "Введите второе число: " )
+
+funcTEST(x, y)
